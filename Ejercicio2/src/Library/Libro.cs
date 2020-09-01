1a77192d897d34bf9651111767e6398822a92cc5
Nicolas Cartalla <nicolas.cartalla@nexa.com.uy>
2020-09-01 20:42:05 -0300

    Comentarios ejercicio 2

--- Ejercicio2/src/Library/Libro.cs
+++ Ejercicio2/src/Library/Libro.cs
@@ -6,18 +6,18 @@ namespace SRP
     {
 
         public string Titulo { get ; }
-        public string Autor { get ; }
-        public string Codigo { get ;  }
+        public string Autor { get ; } 
+        public string Codigo { get ;  } 
         public string SectorBiblioteca { get ; set; } // Hay que crear un nuevo Objeto bibloteca y en ella se debe almacenar el libro, por lo tanto se crea una nueva clase Bibloteca.cs
         public string EstanteBiblioteca { get ; set; }
-
+                                                    // Se debe de crear un objeto y clase Autor
         public Libro(String titulo, String autor, String codigo)
         {
             this.Titulo = titulo;
             this.Autor = autor;
             this.Codigo = codigo;
         }
-
+                                                
         public void AlmacenarLibro(String sector, String estante)
         {
             this.SectorBiblioteca = sector;
