af924f35db5cd9ce6380af1a15d53e568286158c
Nicolas Cartalla <nicolas.cartalla@nexa.com.uy>
2020-09-01 19:28:19 -0300

    Comentarios ejercicio 1

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Expert_SRP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alfajor a = new Alfajor(10, 8.5);
+            Kiosco k = new Kiosco();
+            if (k.PuedeComprar(a, 15, "$")) //Se debe de crear una clase saldo.cs para corroborar el credito de la persona
+            {
+                Console.WriteLine("Felicitaciones! Tiene suficiente dinero para comprar un alfajor :)");
+            }
+            else
+            {
+                Console.WriteLine("Mejor consiga un trabajo :(");
+            }
+        }
+    }
+}
---
+++ Ejercicio1/src/Library/Kiosco.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Expert_SRP
+{
+    public class Kiosco 
+    {
+        public Boolean PuedeComprar(Alfajor a, Double dinero, String moneda) 
+        {
+            Double pesos = ConvertirAPesos(dinero, moneda);
+            return pesos >= a.PrecioDulce + a.PrecioMasa;
+        }
+        private Double ConvertirAPesos(Double dinero, String moneda) //Se debe de crear una clase conertirapes.cs encargada de realizar la conversión a pesos
+        {
+            if (moneda.Equals("U$S")) 
+            {
+                return dinero / 30;
+            } 
+            else if (moneda.Equals("$")) 
+            {
+                return dinero;
+            }
+            else 
+            {
+                return -1;
+            }
+        }
+    }
+}
