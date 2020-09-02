d662ebad21a5083debbc8c84af13f94e4dc852f5
Nicolas Cartalla <nicolas.cartalla@nexa.com.uy>
2020-09-01 21:01:29 -0300

    Comentarios ejercicio 3

---
+++ Ejercicio3/src/Library/AppointmentService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+
+namespace Library
+{
+    public class AppointmentService
+    {
+            // se debe de separar el checkeo de la información al crear la cita
+            // por lo tanto el servicio de citas podría tener algúna otra función como el de cancelar
+            // Con esto se separa la responsabilidad citas con la de verificaciones
+        public static string CreateAppointment(string name, string id, string phoneNumber, DateTime date, string appoinmentPlace, string doctorName)
+        {
+            StringBuilder stringBuilder = new StringBuilder("Scheduling appointment...\n");
+            Boolean isValid = true;
+
+            if (string.IsNullOrEmpty(name))
+            {
+                stringBuilder.Append("Unable to schedule appointment, Name is required\n");
+                isValid = false;
+            }
+
+            if (string.IsNullOrEmpty(id))
+            {
+                stringBuilder.Append("Unable to schedule appointment, id is required\n");
+                isValid = false;
+            }
+
+            if (string.IsNullOrEmpty(phoneNumber))
+            {
+                stringBuilder.Append("Unable to schedule appointment, Phone number is required\n");
+                isValid = false;
+            }
+
+            if (string.IsNullOrEmpty(appoinmentPlace))
+            {
+                stringBuilder.Append("Unable to schedule appointment, Appoinment place is required\n");
+                isValid = false;
+            }
+
+            
+            if (string.IsNullOrEmpty(doctorName))
+            {
+                stringBuilder.Append("Unable to schedule appointment, Doctor name is required\n");
+                isValid = false;
+            }
+
+            if (isValid)
+            {
+                stringBuilder.Append("Appoinment Scheduled");
+            }
+
+            return stringBuilder.ToString();
+        }
+
+    }
+}
