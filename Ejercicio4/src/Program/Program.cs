5bbe1f53115dd6bf241b5436f7b1a57b6cb4c211
Nicolas Cartalla <nicolas.cartalla@nexa.com.uy>
2020-09-08 19:48:00 -0300

    sol ej 4

---
+++ Ejercicio4/src/Program/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadFile reader = new ReadFile("../../assets/board.txt");
+            GameBoard board = new GameBoard(reader.ReadTextFile());
+
+            PrintBoard.Print(board);
+        }
+    }
+}
